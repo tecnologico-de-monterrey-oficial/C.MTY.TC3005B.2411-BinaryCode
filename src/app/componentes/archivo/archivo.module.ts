72fa3609d8ad4a8f7aa77540828879eb303789f8
Samsbg <samanthasofiabg@gmail.com>
2024-04-06 19:09:14 -0600

    Vista de archivos responsiva

--- src/app/componentes/archivo/archivo.module.ts
+++ src/app/componentes/archivo/archivo.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { ArchivoComponent } from './archivo.component';
 
 import { CommonModule } from '@angular/common';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzTagModule } from 'ng-zorro-antd/tag';
 import { NzGridModule } from 'ng-zorro-antd/grid';
@@ -10,6 +11,7 @@ import { NzGridModule } from 'ng-zorro-antd/grid';
 @NgModule({
   imports: [
     CommonModule,
+    NzDropDownModule,
     NzIconModule,
     NzTagModule,
     NzGridModule,
--- src/app/paginas/MainLayout/contenido/archivos/archivos.module.ts
+++ src/app/paginas/MainLayout/contenido/archivos/archivos.module.ts
@@ -6,13 +6,15 @@ import { ArchivoModule } from '../../../../componentes/archivo/archivo.module';
 import { CommonModule } from '@angular/common';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
 
 @NgModule({
   imports: [
     ArchivoModule,
     CommonModule,
     NzGridModule,
-    NzIconModule
+    NzIconModule,
+    NzDividerModule
   ],
   declarations: [
     ArchivosComponent,
