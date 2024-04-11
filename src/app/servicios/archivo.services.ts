b98686f13c0cee81cae955e2b59bbc6183a44623
Samsbg <samanthasofiabg@gmail.com>
2024-04-11 11:18:57 -0600

    Agregar iconos

--- src/app/servicios/archivo.services.ts
+++ src/app/servicios/archivo.services.ts
@@ -39,9 +39,9 @@ export class ArchivosService {
         new Archivo(true, fileIcons.word, 'Archivo 1', new Usuario('', 'Kraken Dominguez'), [new Etiqueta('Etiqueta 1', tarjeta_morado), new Etiqueta('Etiqueta 2', tarjeta_amarillo), new Etiqueta('Etiqueta 3', tarjeta_azul_medio)], '29 de febrero de 2024'),
         new Archivo(false, fileIcons.pdf, 'Archivo 2', new Usuario('', 'Luis A Escudero'), [new Etiqueta('Etiqueta 4', tarjeta_azul_claro), new Etiqueta('Etiqueta 5', tarjeta_rosa), new Etiqueta('Etiqueta 6', tarjeta_amarillo)], '29 de febrero de 2024'),
         new Archivo(true, fileIcons.excel, 'Archivo 3', new Usuario('', 'Samantha Bautista'), [new Etiqueta('Etiqueta 7', tarjeta_rojo_claro), new Etiqueta('Etiqueta 8', tarjeta_morado_claro), new Etiqueta('Etiqueta 9', tarjeta_rosa)], '29 de febrero de 2024'),
-        new Archivo(false, new Icono(fileIcons.unknown, icono_rosa), 'Archivo 4', new Usuario('', 'Angel Garcia'), [new Etiqueta('Etiqueta 10', tarjeta_verde_fuerte), new Etiqueta('Etiqueta 11', tarjeta_azul_fuerte), new Etiqueta('Etiqueta 12', tarjeta_morado_claro)], '29 de febrero de 2024'),
+        new Archivo(false, fileIcons.solidWorks, 'Archivo 4', new Usuario('', 'Angel Garcia'), [new Etiqueta('Etiqueta 10', tarjeta_verde_fuerte), new Etiqueta('Etiqueta 11', tarjeta_azul_fuerte), new Etiqueta('Etiqueta 12', tarjeta_morado_claro)], '29 de febrero de 2024'),
         new Archivo(true, fileIcons.ppt, 'Archivo 5', new Usuario('', 'Carlos Mallén'), [new Etiqueta('Etiqueta 13', tarjeta_rojo_medio), new Etiqueta('Etiqueta 14', tarjeta_verde_claro), new Etiqueta('Etiqueta 15', tarjeta_azul_fuerte)], '29 de febrero de 2024'),
-        new Archivo(false, fileIcons.jpg, 'Archivo 6', new Usuario('', 'Presno Gonzalez'), [new Etiqueta('Etiqueta 16', tarjeta_amarillo_fuerte), new Etiqueta('Etiqueta 17', tarjeta_azul_medio), new Etiqueta('Etiqueta 18', tarjeta_verde_claro)], '29 de febrero de 2024'),
+        new Archivo(false, fileIcons.image, 'Archivo 6', new Usuario('', 'Presno Gonzalez'), [new Etiqueta('Etiqueta 16', tarjeta_amarillo_fuerte), new Etiqueta('Etiqueta 17', tarjeta_azul_medio), new Etiqueta('Etiqueta 18', tarjeta_verde_claro)], '29 de febrero de 2024'),
         ];
 
     getArchivos() {
