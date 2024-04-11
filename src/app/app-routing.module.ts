079b2e2083c63cfbdcc536b4de4387d7ae839638
Escudero <A00832309@tec.mx>
2024-04-11 11:49:10 -0600

    Bases del ruteo
    Solamente se agregaron dos formas de ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { MainLayoutComponent } from './paginas/MainLayout/main-layout.component';
 const routes: Routes = [
+  // { path: 'inicio', component: MainLayoutComponent },
+  // { path: '', redirectTo: 'inicio' }
   { path: '', pathMatch: 'full', redirectTo: '/inicio' },
   { path: 'inicio', loadChildren: () => import('./paginas/MainLayout/main-layout.module').then(m => m.MainLayoutModule) },
   { path: 'entrada', loadChildren: () => import('./paginas/Entrada/entrada.module').then(m => m.EntradaModule) },
+  { path: 'archivos', loadChildren: () => import('./paginas/MainLayout/contenido/archivos/archivos.module').then(m => m.ArchivosModule) },  
 ];
 
 @NgModule({
