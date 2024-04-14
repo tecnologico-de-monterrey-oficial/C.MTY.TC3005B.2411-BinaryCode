e517d568f22963c43fc45b4971851e6c3e9c3d45
CMallen22 <114451529+CMallen22@users.noreply.github.com>
2024-04-14 12:44:17 -0600

    Log in en componente y register tambien :D

---
+++ src/app/EntradaLogin/Registro/registro.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-registro',
+    templateUrl: './registro.component.html',
+    styleUrls: ['./registro.component.css']
+})
+
+export class RegistroComponent {
+    colorSeleccionado: string | null = null;
+
+    seleccionarColor(color: string) {
+        this.colorSeleccionado = this.colorSeleccionado === color ? null : color;
+    }
+}
