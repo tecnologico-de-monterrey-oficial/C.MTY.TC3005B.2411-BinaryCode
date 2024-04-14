e71f0c9b571a6ed473ace80b5468c6e76c445862
CMallen22 <114451529+CMallen22@users.noreply.github.com>
2024-04-14 12:44:17 -0600

    Log in en componente y register tambien :D

--- src/app/EntradaLogin/entrada-login.module.ts
+++ src/app/EntradaLogin/entrada-login.module.ts
@@ -4,11 +4,14 @@ import { EntradaLoginRoutingModule } from './entrada-login-routing.module';
 
 import { EntradaLoginComponent } from './entrada-login.component';
 
+import { LoginComponent } from './Log in/Login.component';
+import { RegistroComponent } from './Registro/registro.component';
+
 @NgModule({
   imports: [
     EntradaLoginRoutingModule,
   ],
-  declarations: [EntradaLoginComponent],
+  declarations: [EntradaLoginComponent, LoginComponent, RegistroComponent],
   exports: [EntradaLoginComponent],
 })
 export class EntradaLoginModule { }
