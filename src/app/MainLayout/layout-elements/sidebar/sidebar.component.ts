0d950d79547a8b4368e70427af1d50c762b80bac
Escudero <A00832309@tec.mx>
2024-04-14 18:24:54 -0600

    Sidebar Component yamamalon

--- src/app/MainLayout/layout-elements/sidebar/sidebar.component.ts
+++ src/app/MainLayout/layout-elements/sidebar/sidebar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { navBarData } from './nav-data';
 
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-	collapsed = false; 
-	navData = navBarData;
+	isCollapsed = false; 
+  
 }
