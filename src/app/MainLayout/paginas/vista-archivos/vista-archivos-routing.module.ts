d09c736d356edc2d0ca2c6cd73a2429386d54f2d
Sebastián Presno <sebastiandash@icloud.com>
2024-04-17 10:52:24 -0600

    Implemented sidebar's ability to change screens on click. Missing Favoritos and recientes

--- src/app/MainLayout/paginas/vista-archivos/vista-archivos-routing.module.ts
+++ src/app/MainLayout/paginas/vista-archivos/vista-archivos-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContenidosComponent } from './contenidos/contenidos.component'; // Importar el componente
+import { FavoritosComponent } from './favoritos/favoritos.component';
+import { RecientesComponent } from './recientes/recientes.component';
 
 const routes: Routes = [
   { path: '', component: ContenidosComponent }, // Esta es la ruta por defecto
+  {path: '/favoritos', component : FavoritosComponent}, 
+  {path: '/recientes', component : RecientesComponent}
 ];
 
 @NgModule({
