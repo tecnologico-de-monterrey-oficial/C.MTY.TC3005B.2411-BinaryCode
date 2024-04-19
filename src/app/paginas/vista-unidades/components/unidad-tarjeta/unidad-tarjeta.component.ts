e13aad967b62aa7c4155a26c184830db15de7bfc
Samsbg <samanthasofiabg@gmail.com>
2024-04-19 09:16:54 -0600

    Eslint #1

--- src/app/paginas/vista-unidades/components/unidad-tarjeta/unidad-tarjeta.component.ts
+++ src/app/paginas/vista-unidades/components/unidad-tarjeta/unidad-tarjeta.component.ts
@@ -11,7 +11,7 @@ export class UnidadTarjetaComponent implements OnInit {
 
   @Input() unidad: Unidad;
 
-  showPlaceholder : Boolean = false;
+  showPlaceholder : boolean = false;
 
   constructor() { }
   
