ebac15d02a6961e3469e2e67bb17bd0b076ee50f
Samsbg <samanthasofiabg@gmail.com>
2024-04-19 10:23:24 -0600

    Eslint #10

--- src/app/MainLayout/main-layout.module.ts
+++ src/app/MainLayout/main-layout.module.ts
@@ -1,48 +1,21 @@
 import { NgModule } from '@angular/core';
 
 import { MainLayoutRoutingModule } from './main-layout-routing.module';
-import { MainLayoutComponent } from './main-layout.component';
-
-import { EncabezadoComponent } from '../components/layout-elements/encabezado/encabezado.component';
-import { SidebarComponent } from '../components/layout-elements/sidebar/sidebar.component';
 
 import { VistaArchivosModule } from '../paginas/vista-archivos/vista-archivos.module';
-
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { VistaUnidadesModule } from '../paginas/vista-unidades/vista-unidades.module';
+import { VistaPersonasModule } from "../paginas/vista-personas/vista-persona.module";
 import { VistaProyectosModule } from "../paginas/vista-proyectos/vista-proyectos.module";
-
-import { CrearContenidosComponent } from "../paginas/vista-archivos/contenidos/crear-contenidos/crear-contenidos.component";
-import { ProyectoCrearComponent } from "../paginas/vista-proyectos/contenido-proyectos/proyecto-crear/proyecto-crear.component";
-import { CrearUnidadComponent } from "../paginas/vista-unidades/components/crear-unidad/crear-unidad.component";
-import {VistaPersonasModule} from "../paginas/vista-personas/vista-persona.module";
+import { VistaUnidadesModule } from '../paginas/vista-unidades/vista-unidades.module';
 
 @NgModule({
     imports: [
-        // MainLayoutRoutingModule,
+      MainLayoutRoutingModule,
 
-        // VistaArchivosModule,
-        // VistaUnidadesModule,
-
-        // NzIconModule,
-        // NzInputModule,
-        // NzLayoutModule,
-        // NzMenuModule,
-        // VistaProyectosModule,
-        // VistaPersonasModule,
+      VistaArchivosModule,
+      VistaPersonasModule,
+      VistaProyectosModule,
+      VistaUnidadesModule,      
     ],
-  declarations: [
-    // MainLayoutComponent,
-    // CrearContenidosComponent,
-    // ProyectoCrearComponent,
-    // CrearUnidadComponent,
-    // CrearContenidosComponent,
-    // ProyectoCrearComponent,
-    // CrearUnidadComponent,
-  ],
-  //exports: [MainLayoutComponent],
+  declarations: [],
 })
 export class MainLayoutModule { }
