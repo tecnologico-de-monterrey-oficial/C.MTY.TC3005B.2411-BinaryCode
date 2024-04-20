bbc1f1210b81ae88e0efe7790cd8115f41339c94
Samsbg <samanthasofiabg@gmail.com>
2024-04-19 20:33:21 -0600

    Hacer registro eficiente

--- src/app/EntradaLogin/entrada-login.module.ts
+++ src/app/EntradaLogin/entrada-login.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { EntradaLoginRoutingModule } from './entrada-login-routing.module';
 
@@ -6,9 +7,10 @@ import { EntradaLoginComponent } from './entrada-login.component';
 
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @NgModule({
-    imports: [EntradaLoginRoutingModule],
+    imports: [EntradaLoginRoutingModule, NzGridModule, CommonModule],
     declarations: [EntradaLoginComponent, LoginComponent, RegistroComponent],
     exports: [EntradaLoginComponent],
 })
--- src/app/EntradaLogin/registro/registro.component.ts
+++ src/app/EntradaLogin/registro/registro.component.ts
@@ -1,4 +1,24 @@
 import { Component } from '@angular/core';
+import {
+    tarjeta_amarillo,
+    tarjeta_amarillo_claro,
+    tarjeta_amarillo_fuerte,
+    tarjeta_azul_claro,
+    tarjeta_azul_fuerte,
+    tarjeta_azul_medio,
+    tarjeta_morado,
+    tarjeta_morado_claro,
+    tarjeta_morado_fuerte,
+    tarjeta_rojo_claro,
+    tarjeta_rojo_fuerte,
+    tarjeta_rojo_medio,
+    tarjeta_rosa,
+    tarjeta_rosa_claro,
+    tarjeta_rosa_fuerte,
+    tarjeta_verde_claro,
+    tarjeta_verde_fuerte,
+    tarjeta_verde_medio,
+} from '../../../assets/colores';
 
 @Component({
     selector: 'app-registro',
@@ -7,6 +27,27 @@ import { Component } from '@angular/core';
 })
 export class RegistroComponent {
     colorSeleccionado: string | null = null;
+
+    colores: string[] = [
+        tarjeta_azul_fuerte,
+        tarjeta_azul_medio,
+        tarjeta_azul_claro,
+        tarjeta_rojo_fuerte,
+        tarjeta_rojo_medio,
+        tarjeta_rojo_claro,
+        tarjeta_morado_fuerte,
+        tarjeta_morado,
+        tarjeta_morado_claro,
+        tarjeta_verde_fuerte,
+        tarjeta_verde_medio,
+        tarjeta_verde_claro,
+        tarjeta_amarillo_fuerte,
+        tarjeta_amarillo,
+        tarjeta_amarillo_claro,
+        tarjeta_rosa_fuerte,
+        tarjeta_rosa,
+        tarjeta_rosa_claro,
+    ];
 
     seleccionarColor(color: string): void {
         this.colorSeleccionado =
