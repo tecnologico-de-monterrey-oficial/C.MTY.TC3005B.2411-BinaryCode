cee117d4d9aeda4d41cf813d3400d51e957d1f0a
Samsbg <samanthasofiabg@gmail.com>
2024-05-03 16:08:35 -0600

    sidebar v1

--- src/app/paginas/vista-permisos/components/sidebar-permisos/sidebar-permisos.component.ts
+++ src/app/paginas/vista-permisos/components/sidebar-permisos/sidebar-permisos.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Unidad } from '../../../../modelos/unidad.model';
+import { Proyecto } from '../../../../modelos/proyectos.model';
+import { US4, US5, US6 } from '../../../../../assets/mocks/usuarios';
+import { Usuario } from '../../../../modelos/usuario.model';
 
 @Component({
     selector: 'app-sidebar-permisos',
     templateUrl: './sidebar-permisos.component.html',
     styleUrl: './sidebar-permisos.component.css',
 })
-export class SidebarPermisosComponent {}
+export class SidebarPermisosComponent {
+    @Input() unidad: Unidad;
+    @Input() proyecto: Proyecto;
+
+    coordinadores: Usuario[] = [US4, US5, US6];
+
+    onRemoveItemClick(idEliminado: string): void {
+        // TODO: Implementar eliminación de usuario API
+        this.coordinadores = this.coordinadores.filter(
+            coordinador => coordinador.id !== idEliminado
+        );
+        console.log('Coordinador eliminado');
+    }
+}
--- src/app/paginas/vista-permisos/vista-permisos.module.ts
+++ src/app/paginas/vista-permisos/vista-permisos.module.ts
@@ -2,16 +2,29 @@ import { NgModule } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { NzGridModule } from 'ng-zorro-antd/grid';
-import { PermisosComponent } from './permisos/permisos.component';
-import { SidebarPermisosComponent } from './components/sidebar-permisos/sidebar-permisos.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
+
+import { VistaUnidadesModule } from '../vista-unidades/vista-unidades.module';
+
 import { EditoresPermisosComponent } from './components/editores-permisos/editores-permisos.component';
+import { MiniCoordinadorComponent } from './components/mini-coordinador/mini-coordinador.component';
+import { SidebarPermisosComponent } from './components/sidebar-permisos/sidebar-permisos.component';
+import { PermisosComponent } from './permisos/permisos.component';
 
 @NgModule({
-    imports: [CommonModule, NzGridModule],
+    imports: [
+        CommonModule,
+        NzGridModule,
+        NzIconModule,
+        NzPopoverModule,
+        VistaUnidadesModule,
+    ],
     declarations: [
-        PermisosComponent,
-        SidebarPermisosComponent,
         EditoresPermisosComponent,
+        MiniCoordinadorComponent,
+        SidebarPermisosComponent,
+        PermisosComponent,
     ],
     exports: [PermisosComponent],
 })
