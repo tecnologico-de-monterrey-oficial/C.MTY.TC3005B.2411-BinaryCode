327b29f57ce4039165f64e0d0ea364c7014499b3
Samsbg <samanthasofiabg@gmail.com>
2024-05-02 21:23:05 -0600

    Mockups

--- src/app/servicios/unidad.services.ts
+++ src/app/servicios/unidad.services.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Unidad } from '../modelos/unidad.model';
-import { U1, U2, U3, U4, U5, U6 } from '../../assets/mocks/unidades';
+import { UN1, UN2, UN3, UN4, UN5, UN6 } from '../../assets/mocks/unidades';
 
 @Injectable({
     providedIn: 'root',
 })
 export class UnidadesService {
-    unidades: Unidad[] = [U1, U2, U3, U4, U5, U6];
+    unidades: Unidad[] = [UN1, UN2, UN3, UN4, UN5, UN6];
 
     getUnidades(): Unidad[] {
         return this.unidades;
