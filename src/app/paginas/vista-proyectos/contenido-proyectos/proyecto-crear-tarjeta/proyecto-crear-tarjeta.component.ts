4e2da1dcafeb6cfa95900e01b48cc1476afd8f2a
Samsbg <samanthasofiabg@gmail.com>
2024-05-04 19:53:35 -0600

    Crear proyecto tarjeta

---
+++ src/app/paginas/vista-proyectos/contenido-proyectos/proyecto-crear-tarjeta/proyecto-crear-tarjeta.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-proyecto-crear-tarjeta',
+    templateUrl: './proyecto-crear-tarjeta.component.html',
+    styleUrl: './proyecto-crear-tarjeta.component.css',
+})
+export class ProyectoCrearTarjetaComponent {}
