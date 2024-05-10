8a8c9a6b8f0ed0fdf71e87e890d08c8cf995d16e
Sebastián Presno <sebastiandash@icloud.com>
2024-05-10 17:32:58 -0600

    Removed an unused import

--- src/app/paginas/vista-unidades/vista-unidades-routing.module.ts
+++ src/app/paginas/vista-unidades/vista-unidades-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UnidadesComponent } from './unidades/unidades.component'; // Importar el componente
-import { ContenidosComponent } from '../vista-archivos/contenidos/contenidos.component';
 
 const routes: Routes = [
     { path: 'unidades/:id', component: UnidadesComponent },
