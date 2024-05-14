88ea383984e6ae8ad5af06074159c4c45247b2e3
Samsbg <samanthasofiabg@gmail.com>
2024-05-14 09:43:26 -0600

    Cambios

--- src/assets/mocks/archivos.ts
+++ src/assets/mocks/archivos.ts
@@ -22,62 +22,102 @@ import {
 } from './etiquetas';
 import { US1, US2, US3, US4, US5, US6 } from './usuarios';
 
-export const A1: Archivo = new Archivo(
-    'A1',
-    true,
-    fileIcons.word,
-    'Archivo 1',
-    US1,
-    [E12, E6, E17],
-    '29 de febrero de 2024'
-);
+export const A1: Archivo = {
+    id: 'A1',
+    favorito: true,
+    icono: fileIcons.word,
+    nombre: 'Archivo 1',
+    propietario: US1,
+    etiquetas: [E12, E6, E17],
+    fecha: '29 de febrero de 2024',
+};
 
-export const A2: Archivo = new Archivo(
-    'A2',
-    false,
-    fileIcons.pdf,
-    'Archivo 2',
-    US2,
-    [E3, E14, E10],
-    '29 de febrero de 2024'
-);
+export const A2: Archivo = {
+    id: 'A2',
+    favorito: false,
+    icono: fileIcons.pdf,
+    nombre: 'Archivo 2',
+    propietario: US2,
+    etiquetas: [E3, E14, E10],
+    fecha: '29 de febrero de 2024',
+};
 
-export const A3: Archivo = new Archivo(
-    'A3',
-    true,
-    fileIcons.excel,
-    'Archivo 3',
-    US3,
-    [E2, E9, E16],
-    '29 de febrero de 2024'
-);
+export const A3: Archivo = {
+    id: 'A3',
+    favorito: true,
+    icono: fileIcons.excel,
+    nombre: 'Archivo 3',
+    propietario: US3,
+    etiquetas: [E2, E9, E16],
+    fecha: '29 de febrero de 2024',
+};
 
-export const A4: Archivo = new Archivo(
-    'A4',
-    false,
-    fileIcons.solidWorks,
-    'Archivo 4',
-    US4,
-    [E8, E4, E15],
-    '29 de febrero de 2024'
-);
+export const A4: Archivo = {
+    id: 'A4',
+    favorito: false,
+    icono: fileIcons.solidWorks,
+    nombre: 'Archivo 4',
+    propietario: US4,
+    etiquetas: [E8, E4, E15],
+    fecha: '29 de febrero de 2024',
+};
 
-export const A5: Archivo = new Archivo(
-    'A5',
-    true,
-    fileIcons.ppt,
-    'Archivo 5',
-    US5,
-    [E7, E18, E11],
-    '29 de febrero de 2024'
-);
+export const A5: Archivo = {
+    id: 'A5',
+    favorito: true,
+    icono: fileIcons.ppt,
+    nombre: 'Archivo 5',
+    propietario: US5,
+    etiquetas: [E7, E18, E11],
+    fecha: '29 de febrero de 2024',
+};
 
-export const A6: Archivo = new Archivo(
-    'A6',
-    false,
-    fileIcons.image,
-    'Archivo 6',
-    US6,
-    [E5, E1, E13],
-    '29 de febrero de 2024'
-);
+export const A6: Archivo = {
+    id: 'A6',
+    favorito: false,
+    icono: fileIcons.image,
+    nombre: 'Archivo 6',
+    propietario: US6,
+    etiquetas: [E5, E1, E13],
+    fecha: '29 de febrero de 2024',
+};
+
+export const A7: Archivo = {
+    id: 'A7',
+    favorito: true,
+    icono: fileIcons.word,
+    nombre: 'Archivo 7',
+    propietario: US1,
+    etiquetas: [E12, E6, E17],
+    fecha: '29 de febrero de 2024',
+};
+
+export const A8: Archivo = {
+    id: 'A8',
+    favorito: true,
+    icono: fileIcons.pdf,
+    nombre: 'Archivo 8',
+    propietario: US2,
+    etiquetas: [E3, E14, E10],
+    fecha: '29 de febrero de 2024',
+};
+
+export const A9: Archivo = {
+    id: 'A9',
+    favorito: true,
+    icono: fileIcons.excel,
+    nombre: 'Archivo 9',
+    propietario: US4,
+    etiquetas: [E2, E9, E16],
+    fecha: '29 de febrero de 2024',
+};
+
+export const A10: Archivo = {
+    id: 'A10',
+    favorito: true,
+    icono: fileIcons.image,
+    nombre: 'Archivo 10',
+    propietario: US6,
+    etiquetas: [E8, E4, E15],
+    fecha: '29 de febrero de 2024',
+};
