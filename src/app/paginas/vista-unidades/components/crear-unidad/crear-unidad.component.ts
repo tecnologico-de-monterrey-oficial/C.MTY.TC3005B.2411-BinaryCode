64a48e369ed8b59d29829c9fe5f616a0256b22b9
Inge Garcia <angel_rgg@hotmail.com>
2024-05-20 15:15:10 -0600

    edicion de unidades

--- src/app/paginas/vista-unidades/components/crear-unidad/crear-unidad.component.ts
+++ src/app/paginas/vista-unidades/components/crear-unidad/crear-unidad.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {
     tarjeta_amarillo,
@@ -21,6 +21,7 @@ import {
     tarjeta_verde_medio,
 } from '../../../../../assets/colores';
 import { ModalDataService } from '../../../../servicios/modal-data.service';
+import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 
 @Component({
@@ -28,8 +29,11 @@ import { Router } from '@angular/router';
     templateUrl: './crear-unidad.component.html',
     styleUrls: ['./crear-unidad.component.css'],
 })
-export class CrearUnidadComponent {
+export class CrearUnidadComponent implements OnInit {
+    subscription: Subscription;
     unidadForm: FormGroup;
+    unidadId: number;
+    color: string = '#000000';
     colores: string[] = [
         tarjeta_amarillo,
         tarjeta_amarillo_claro,
@@ -53,6 +57,7 @@ export class CrearUnidadComponent {
     colorSeleccionado: string;
     imagenURL: string | ArrayBuffer | null = null;
     modo: string = 'crear';
+    imagenOriginal: string;
     constructor(
         private fb: FormBuilder,
         private modalDataService: ModalDataService,
@@ -62,6 +67,25 @@ export class CrearUnidadComponent {
             nombreUnidad: ['', Validators.required],
             color: ['', Validators.required],
             imagen: [null, Validators.required],
+        });
+    }
+
+    ngOnInit(): void {
+        this.subscription = this.modalDataService.unidadData.subscribe(data => {
+            if (data) {
+                this.unidadId = data.id;
+                this.modo = 'editar';
+                this.color = data.color;
+                this.imagenURL = data.imagen;
+                this.imagenOriginal = data.imagen;
+                this.unidadForm.patchValue({
+                    nombreUnidad: data.nombre,
+                    color: data.color,
+                    imagen: data.imagen,
+                });
+            } else {
+                this.color = this.colores[0];
+            }
         });
     }
 
@@ -80,50 +104,84 @@ export class CrearUnidadComponent {
     validarDatos(): void {
         const ruta: string = this.router.url;
         const idProyecto: number = this.extraerNumero(ruta);
-        //const date: Date = new Date();
-        if (this.unidadForm.valid) {
+        if (this.modo === 'crear') {
+            if (this.unidadForm.valid) {
+                // eslint-disable-next-line @typescript-eslint/typedef
+                const unidadData = {
+                    nombre: this.unidadForm.get('nombreUnidad').value,
+                    color: this.unidadForm.get('color').value,
+                    imagen: this.unidadForm.get('imagen').value,
+                    //fecha: date,
+                    id_padre: null,
+                    id_proyecto: idProyecto,
+                    id_usuario: 1, //TODO se debe sacar desde que se inicia la app
+                };
+                fetch('http://127.0.0.1:8000/api/apartados/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(unidadData),
+                })
+                    .then(response => response.json())
+                    .then(proyectoData => {
+                        console.log('Success:', proyectoData);
+                        setTimeout(function () {
+                            location.reload();
+                        }, 500);
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                        alert('Error al crear la unidad.');
+                    });
+            } else {
+                alert('Por favor completa todos los campos.');
+            }
+        } else {
             // eslint-disable-next-line @typescript-eslint/typedef
-            const unidadData = {
+            const unidadEdit = {
                 nombre: this.unidadForm.get('nombreUnidad').value,
                 color: this.unidadForm.get('color').value,
-                imagen: this.unidadForm.get('imagen').value,
-                //fecha: date,
-                id_padre: null,
+                imagen: this.imagenEditada()
+                    ? this.unidadForm.get('imagen').value
+                    : undefined,
+                id_usuario: 1, //TODO
                 id_proyecto: idProyecto,
-                id_usuario: 1, //TODO se debe sacar desde que se inicia la app
             };
-            fetch('http://127.0.0.1:8000/api/apartados/', {
-                method: 'POST',
+            fetch(`http://127.0.0.1:8000/api/apartados/${this.unidadId}/`, {
+                // Reemplaza con tu URL de la API
+                method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(unidadData),
+                body: JSON.stringify(unidadEdit),
             })
                 .then(response => response.json())
                 .then(proyectoData => {
                     console.log('Success:', proyectoData);
-                    setTimeout(function () {
+                    /* setTimeout(function () {
                         location.reload();
-                    }, 500);
+                    }, 500);*/
                 })
                 .catch(error => {
                     console.error('Error:', error);
-                    alert('Error al crear la unidad.');
+                    alert('Error al editar la unidad.');
                 });
-        } else {
-            alert('Por favor completa todos los campos.');
         }
-    }
-
-    extraerNumero(ruta: string): number {
-        const match: RegExpMatchArray = ruta.match(
-            /\/proyectos\/(\d+)\/unidades/
-        );
-        return match ? parseInt(match[1], 10) : null;
     }
 
     seleccionarColor(color: string): void {
         this.colorSeleccionado = color;
         this.unidadForm.patchValue({ color: this.colorSeleccionado });
+    }
+    imagenEditada(): boolean {
+        // Comprueba si la URL actual de la imagen es diferente de la URL original
+        return this.unidadForm.get('imagen').value !== this.imagenOriginal;
+    }
+    extraerNumero(ruta: string): number {
+        const match: RegExpMatchArray = ruta.match(
+            /\/proyectos\/(\d+)\/unidades/
+        );
+        return match ? parseInt(match[1], 10) : null;
     }
 }
