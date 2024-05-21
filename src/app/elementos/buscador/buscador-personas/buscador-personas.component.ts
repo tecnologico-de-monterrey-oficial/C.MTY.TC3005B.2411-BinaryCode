02868036752d0b2e3b63257b7884fcbbe02b4bca
Samsbg <samanthasofiabg@gmail.com>
2024-05-21 13:09:10 -0600

    Buscador algoritmo

--- src/app/elementos/buscador/buscador-personas/buscador-personas.component.ts
+++ src/app/elementos/buscador/buscador-personas/buscador-personas.component.ts
@@ -2,6 +2,8 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { UsuariosServices } from '../../../servicios/usuarios.services';
 import { personaBuscador } from '../buscador-mini-personas/buscador-mini-personas.component';
 import { Usuario } from '../../../modelos/usuario.model';
+import { FuseResult } from 'fuse.js';
+import Fuse from 'fuse.js';
 
 @Component({
     selector: 'app-buscador-personas',
@@ -12,12 +14,33 @@ export class BuscadorPersonasComponent {
     @Output() agregarCoordinadores = new EventEmitter<Usuario[]>();
     @Output() cancelar = new EventEmitter<void>();
 
-    seleccionados: personaBuscador[];
+    fuse: Fuse<personaBuscador>;
+    usuariosTotales: personaBuscador[];
+    usuariosFiltrados: personaBuscador[];
+
+    parametrosBusqueda = {
+        threshold: 0.3,
+        ignoreLocation: true,
+        keys: ['nombre'],
+    };
 
     constructor(usuariosService: UsuariosServices) {
-        this.seleccionados = usuariosService
+        this.usuariosTotales = usuariosService
             .getUsuarios()
             .map(usuario => ({ ...usuario, seleccionado: false }));
+        this.sort();
+
+        this.usuariosFiltrados = this.usuariosTotales;
+        this.fuse = new Fuse(this.usuariosTotales, this.parametrosBusqueda);
+    }
+
+    sort(): void {
+        this.usuariosTotales.sort((a, b) => {
+            if (a.seleccionado === b.seleccionado) {
+                return a.nombre.localeCompare(b.nombre);
+            }
+            return b.seleccionado ? 1 : -1;
+        });
     }
 
     handleCancel(): void {
@@ -25,13 +48,14 @@ export class BuscadorPersonasComponent {
     }
 
     handleSeleccionar(persona: personaBuscador): void {
-        this.seleccionados = this.seleccionados.map(p =>
+        this.usuariosTotales = this.usuariosTotales.map(p =>
             p === persona ? { ...p, seleccionado: !p.seleccionado } : p
         );
+        this.sort();
     }
 
     handleAgregar(): void {
-        const finales: Usuario[] = this.seleccionados
+        const finales: Usuario[] = this.usuariosTotales
             .filter(p => p.seleccionado)
             .map(p => ({
                 id: p.id,
@@ -40,5 +64,15 @@ export class BuscadorPersonasComponent {
                 fecha: p.fecha,
             }));
         this.agregarCoordinadores.emit(finales);
+    }
+
+    handleSearch(termino: string): void {
+        if (!termino) {
+            this.usuariosFiltrados = this.usuariosTotales;
+        } else {
+            const result: FuseResult<personaBuscador>[] =
+                this.fuse.search(termino);
+            this.usuariosFiltrados = result.map(resultado => resultado.item);
+        }
     }
 }
