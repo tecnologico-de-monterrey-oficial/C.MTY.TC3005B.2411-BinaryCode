5ec1c282a19604ce81d59fe4064c830aa7a2563e
CMallen22 <114451529+CMallen22@users.noreply.github.com>
2024-06-07 00:50:14 -0600

    No hecho bien la lista de colores,no me funcionaba

--- src/app/paginas/vista-archivos/components/crear-carpeta/crear-carpeta.component.ts
+++ src/app/paginas/vista-archivos/components/crear-carpeta/crear-carpeta.component.ts
@@ -1,6 +1,27 @@
-// crear-carpeta.component.ts
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+// Importaciones de los colores desde el archivo correspondiente
+import {
+    tarjeta_amarillo,
+    tarjeta_amarillo_claro,
+    tarjeta_amarillo_fuerte,
+    tarjeta_azul_claro,
+    tarjeta_azul_fuerte,
+    tarjeta_azul_medio,
+    tarjeta_morado,
+    tarjeta_morado_claro,
+    tarjeta_morado_fuerte,
+    tarjeta_rojo_claro,
+    tarjeta_rojo_fuerte,
+    tarjeta_rojo_medio,
+    tarjeta_rosa,
+    tarjeta_rosa_claro,
+    tarjeta_rosa_fuerte,
+    tarjeta_verde_claro,
+    tarjeta_verde_fuerte,
+    tarjeta_verde_medio,
+} from '../../../../../assets/colores';
 
 @Component({
     selector: 'app-crear-carpeta',
@@ -10,25 +31,48 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class CrearCarpetaComponent implements OnInit {
     unidadForm: FormGroup;
     colorSeleccionado: string;
-    colores: string[] = ['#FF5733', '#33FF57', '#3357FF', '#FFFF33', '#FF33FF']; // Colores de ejemplo
+    colores: string[] = [
+        tarjeta_amarillo,
+        tarjeta_amarillo_claro,
+        tarjeta_amarillo_fuerte,
+        tarjeta_azul_claro,
+        tarjeta_azul_fuerte,
+        tarjeta_azul_medio,
+        tarjeta_morado,
+        tarjeta_morado_claro,
+        tarjeta_morado_fuerte,
+        tarjeta_rojo_claro,
+        tarjeta_rojo_fuerte,
+        tarjeta_rojo_medio,
+        tarjeta_rosa,
+        tarjeta_rosa_claro,
+        tarjeta_rosa_fuerte,
+        tarjeta_verde_claro,
+        tarjeta_verde_fuerte,
+        tarjeta_verde_medio,
+    ];
 
     constructor(private formBuilder: FormBuilder) {}
 
     ngOnInit(): void {
         this.iniciarFormulario();
+        console.log('Colores disponibles:', this.colores); // Añade este logging
     }
 
     iniciarFormulario(): void {
+        // Inicializa el formulario usando formBuilder
         this.unidadForm = this.formBuilder.group({
-            nombreUnidad: ['', Validators.required],
+            nombreCarpeta: ['', Validators.required],
             color: [this.colores[0], Validators.required],
         });
-        // Establece el color predeterminado
+
+        // Establece el color predeterminado en la variable para el binding con la clase CSS
         this.colorSeleccionado = this.colores[0];
     }
 
     seleccionarColor(color: string): void {
+        // Establece el color seleccionado y actualiza el valor del formulario
         this.colorSeleccionado = color;
-        this.unidadForm.patchValue({ color: this.colorSeleccionado });
+        this.unidadForm.patchValue({ color: color });
     }
 }
