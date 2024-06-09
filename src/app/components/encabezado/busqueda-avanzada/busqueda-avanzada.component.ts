a23469f42a5fbedfa9a2358a366da34734281894
CMallen22 <114451529+CMallen22@users.noreply.github.com>
2024-06-09 16:39:28 -0600

    Preparar busqueda avanzada y vista dearchivoslisto

--- src/app/components/encabezado/busqueda-avanzada/busqueda-avanzada.component.ts
+++ src/app/components/encabezado/busqueda-avanzada/busqueda-avanzada.component.ts
@@ -1,14 +1,12 @@
-/*
-import { Component, OnInit } from '@angular/core';
+// busqueda-avanzada.component.ts
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-busqueda-avanzada',
     templateUrl: './busqueda-avanzada.component.html',
     styleUrls: ['./busqueda-avanzada.component.css'],
 })
-export class BusquedaAvanzadaComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit(): void {}
+export class BusquedaAvanzadaComponent {
+    //constructor() {}
+    //ngOnInit(): void {}
 }
-*/
