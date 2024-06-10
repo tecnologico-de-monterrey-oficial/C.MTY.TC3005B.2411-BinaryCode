1dccfaabfcdea4c54d2d26112dcadaca8100f98c
CMallen22 <114451529+CMallen22@users.noreply.github.com>
2024-06-09 19:26:44 -0600

    Busqueda avanzada bases

--- src/app/components/encabezado/busqueda-avanzada/busqueda-avanzada.component.ts
+++ src/app/components/encabezado/busqueda-avanzada/busqueda-avanzada.component.ts
@@ -1,5 +1,21 @@
-// busqueda-avanzada.component.ts
 import { Component } from '@angular/core';
+
+interface Proyecto {
+    id: number;
+    nombre: string;
+    color: string;
+}
+
+interface Etiqueta {
+    id: number;
+    nombre: string;
+    color: string;
+}
+
+interface Persona {
+    id: number;
+    nombre: string;
+}
 
 @Component({
     selector: 'app-busqueda-avanzada',
@@ -7,6 +23,36 @@ import { Component } from '@angular/core';
     styleUrls: ['./busqueda-avanzada.component.css'],
 })
 export class BusquedaAvanzadaComponent {
-    //constructor() {}
-    //ngOnInit(): void {}
+    proyectos: Proyecto[] = [
+        { id: 1, nombre: 'Proyecto A', color: 'red' },
+        { id: 2, nombre: 'Proyecto B', color: 'blue' },
+        { id: 3, nombre: 'Proyecto C', color: 'green' },
+    ];
+    etiquetas: Etiqueta[] = [
+        { id: 1, nombre: 'Etiqueta A', color: 'yellow' },
+        { id: 2, nombre: 'Etiqueta B', color: 'purple' },
+        { id: 3, nombre: 'Etiqueta C', color: 'orange' },
+    ];
+    personas: Persona[] = [
+        { id: 1, nombre: 'Persona A' },
+        { id: 2, nombre: 'Persona B' },
+        { id: 3, nombre: 'Persona C' },
+    ];
+    selectedProyectos: Proyecto[] = [];
+    selectedEtiquetas: Etiqueta[] = [];
+    selectedPersonas: Persona[] = [];
+    fechaInicio: Date | null = null;
+    fechaFin: Date | null = null;
+
+    onProyectoSelect(value: Proyecto[]): void {
+        this.selectedProyectos = value;
+    }
+
+    onEtiquetaSelect(value: Etiqueta[]): void {
+        this.selectedEtiquetas = value;
+    }
+
+    onPersonaSelect(value: Persona[]): void {
+        this.selectedPersonas = value;
+    }
 }
