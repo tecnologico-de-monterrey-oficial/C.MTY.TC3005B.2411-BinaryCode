907ece4873474e12e0707d251de96bc082dacdca
Inge Garcia <angel_rgg@hotmail.com>
2024-06-13 10:43:07 -0600

    acabado

--- src/app/paginas/vista-archivos/components/archivo-completo/archivo-completo.component.ts
+++ src/app/paginas/vista-archivos/components/archivo-completo/archivo-completo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ArchivoModalService } from '../../../../servicios/archivo-modal.service';
 import { Archivo } from '../../../../modelos/archivo.model';
 import { getIcono } from '../../../../modelos/icono.model';
+import { ArchivosService } from '../../../../servicios/archivo.services';
 
 @Component({
     selector: 'app-archivo-completo',
@@ -13,7 +14,10 @@ export class ArchivoCompletoComponent implements OnInit {
     selectedVersion: string = 'Version';
     versions: string[] = ['Version 1.0', 'Version 1.1', 'Version 2.0'];
 
-    constructor(private archivoModalService: ArchivoModalService) {
+    constructor(
+        private archivoModalService: ArchivoModalService,
+        private archivoService: ArchivosService
+    ) {
         this.archivo = this.archivoModalService.getArchivo();
     }
 
@@ -35,6 +39,25 @@ export class ArchivoCompletoComponent implements OnInit {
             month: 'long',
             day: '2-digit',
         });
+    }
+    async downloadFile(): Promise<void> {
+        try {
+            const blob: Blob = await this.archivoService
+                .downloadFile(this.archivo.id)
+                .toPromise();
+            // eslint-disable-next-line @typescript-eslint/typedef
+            const url = window.URL.createObjectURL(blob);
+            // eslint-disable-next-line @typescript-eslint/typedef
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `${this.archivo.nombre}.pdf`; // Puedes cambiar el nombre y extensión según el archivo
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        } catch (error) {
+            console.error('Download error:', error);
+        }
     }
 
     selectVersion(version: string): void {
